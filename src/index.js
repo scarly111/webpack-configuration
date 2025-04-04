148e3b20320a5759cc53bf959c33ae338086dcd3
scarly111 <yigitcagri48@gmail.com>
2025-04-04 20:18:00 +0300

    The Odin Project Webpack lesson

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// index.js
+import "./styles.css";
+import { greeting } from "./greeting.js";
+import odinImage from "./odin.png";
+
+console.log(greeting);
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.textContent = 'Hellooooooooo!'
+   
+document.body.appendChild(image);
